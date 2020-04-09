0c717c0e7397b746d830540cb1ab77a31e01fa47
Scott Pribe <Scottpribe@gmail.com>
2020-04-09 16:39:38 -0400

    JS add,subtract,reset functions work properly. Centered the content in the middle of page.

---
+++ script.js
@@ -0,0 +1,31 @@
+const addition = document.querySelector('#addition');
+const subtraction = document.querySelector('#subtraction');
+const reset = document.querySelector('#reset');
+
+let curValue = 0;
+document.getElementById('displayCount').innerHTML= curValue;
+
+
+addition.addEventListener('click', () => {
+    addCount();
+})
+
+subtraction.addEventListener('click', () => {
+    subtractCount();
+})
+
+reset.addEventListener('click', () => {
+    resetCount();
+})
+
+function addCount(){
+    document.getElementById('displayCount').innerHTML= ++curValue;
+}
+function subtractCount (){
+    document.getElementById('displayCount').innerHTML= --curValue;
+   
+}
+function resetCount (){
+    let curValue = 0;
+    document.getElementById('displayCount').innerHTML= curValue;
+}
